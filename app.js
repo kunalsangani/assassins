60299f3d5c3cd12f768df93049f02aba0e89c4aa
Kunal Sangani <Kunal@rescomp-14-251937.stanford.edu>
2016-01-05 13:36:09 -0800

    Solely using bodyparser

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 })); 
-app.use(express.json());
-app.use(express.urlencoded());
 
 
 app.get('/', function (req, res) {
